2cbf371071dd37dc952b4676fa52b545b53df00a
Lu Weizheng <luweizheng36@hotmail.com>
2020-11-27 15:52:30 +0800

    utils

--- neural-network/cnn/mlutils/pytorch.py
+++ neural-network/cnn/mlutils/pytorch.py
@@ -21,3 +21,74 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNI
     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return train_iter, test_iter
+
+def try_gpu(i=0):
+    """Return gpu device if exists, otherwise return cpu device."""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def accuracy(y_hat, y):
+    """Compute the number of correct predictions."""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = argmax(y_hat, axis=1)        
+    cmp = astype(y_hat, y.dtype) == y
+    return float(reduce_sum(astype(cmp, y.dtype)))
+
+def evaluate_accuracy_gpu(net, data_iter, device=None):
+    """Compute the accuracy for a model on a dataset using a GPU."""
+    net.eval()  # Set the model to evaluation mode
+    if not device:
+        device = next(iter(net.parameters())).device
+    # Accumulator has 2 parameters: (number of correct predictions, number of predictions)
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        X, y = X.to(device), y.to(device)
+        metric.add(accuracy(net(X), y), size(y))
+    return metric[0] / metric[1]
+
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+class Accumulator:
+    """For accumulating sums over n variables."""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+argmax = lambda x, *args, **kwargs: x.argmax(*args, **kwargs)
+astype = lambda x, *args, **kwargs: x.type(*args, **kwargs)
+reduce_sum = lambda x, *args, **kwargs: x.sum(*args, **kwargs)
+size = lambda x, *args, **kwargs: x.numel(*args, **kwargs)
--- neural-network/cnn/pytorch/lenet-fashionmnist.py
+++ neural-network/cnn/pytorch/lenet-fashionmnist.py
@@ -5,30 +5,30 @@ import torchvision
 from torch import nn, optim
 import sys
 sys.path.append("..") 
-import mlutils.pytorch as 
+import mlutils.pytorch as mlutils
 
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
         
-        # 输入 1 * 28 * 28
+        # input shape: 1 * 28 * 28
         self.conv = nn.Sequential(
-            # 卷积层1
-            # 在输入基础上增加了padding，28 * 28 -> 32 * 32
-            # 1 * 32 * 32 -> 6 * 28 * 28
+            # conv layer 1
+            # add padding: 28 * 28 -> 32 * 32
+            # conv: 1 * 32 * 32 -> 6 * 28 * 28
             nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2), nn.Sigmoid(),
             # 6 * 28 * 28 -> 6 * 14 * 14
             nn.MaxPool2d(kernel_size=2, stride=2), # kernel_size, stride
-            # 卷积层2
+            # conv layer 2
             # 6 * 14 * 14 -> 16 * 10 * 10 
             nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5), nn.Sigmoid(),
             # 16 * 10 * 10 -> 16 * 5 * 5
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.fc = nn.Sequential(
-            # 全连接层1
+            # full connect layer 1
             nn.Linear(in_features=16 * 5 * 5, out_features=120), nn.Sigmoid(),
-            # 全连接层2
+            # full connect layer 2
             nn.Linear(in_features=120, out_features=84), nn.Sigmoid(),
             nn.Linear(in_features=84, out_features=10)
         )
@@ -38,6 +38,7 @@ class LeNet(nn.Module):
         output = self.fc(feature.view(img.shape[0], -1))
         return output
 
+# save to mlutils so that other program can use it
 def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNIST'):
     """Use torchvision.datasets module to download the fashion mnist dataset and then load into memory."""
     trans = []
@@ -74,6 +75,7 @@ def evaluate_accuracy(data_iter, net, device=None):
             n += y.shape[0]
     return acc_sum / n
 
+# save to mlutils so that other program can use it
 def try_gpu(i=0):
     if torch.cuda.device_count() >= i + 1:
         return torch.device(f'cuda:{i}')
@@ -84,9 +86,13 @@ def train(net, train_iter, test_iter, batch_size, optimizer, num_epochs, device=
     print("training on", device)
     loss = torch.nn.CrossEntropyLoss()
     batch_count = 0
+    timer, num_batches = mlutils.Timer(), len(train_iter)
     for epoch in range(num_epochs):
+        # Accumulator has 3 parameters: (loss, train_acc, batch_size)
+        metric = mlutils.Accumulator(3)
         train_l_sum, train_acc_sum, n = 0.0, 0.0, 0
         for X, y in train_iter:
+            timer.start()
             X = X.to(device)
             y = y.to(device)
             y_hat = net(X)
@@ -94,13 +100,19 @@ def train(net, train_iter, test_iter, batch_size, optimizer, num_epochs, device=
             optimizer.zero_grad()
             l.backward()
             optimizer.step()
-            train_l_sum += l.cpu().item()
-            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], mlutils.accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            # metric[0] = l * X.shape[0], metric[2] = X.shape[0]
+            train_l = metric[0]/metric[2]
+            # metric[1] = number of correct predictions, metric[2] = X.shape[0]
+            train_acc = metric[1]/metric[2]
             n += y.shape[0]
             batch_count += 1
-        test_acc = evaluate_accuracy(test_iter, net)
-        if epoch % 10 == 0:
+        test_acc = mlutils.evaluate_accuracy_gpu(net, test_iter)
+        if epoch % 1 == 0:
             print(f'epoch {epoch + 1} : loss {train_l_sum / batch_count:.3f}, train acc {train_acc_sum / n:.3f}, test acc {test_acc:.3f}')
+            print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec ' f'on {str(device)}')
 
 def main():
 
