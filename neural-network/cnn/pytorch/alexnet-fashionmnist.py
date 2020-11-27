6962f9ea5229b3bf87ec71efc0b4b53d85b40093
Lu Weizheng <luweizheng36@hotmail.com>
2020-11-27 10:13:12 +0800

    alexnet

--- neural-network/cnn/pytorch/alexnet-fashionmnist.py
+++ neural-network/cnn/pytorch/alexnet-fashionmnist.py
@@ -9,7 +9,7 @@ class AlexNet(nn.Module):
     def __init__(self):
         super(AlexNet, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=4, padding=1), 
+            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=11, stride=4, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2), # kernel_size, stride
             # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
@@ -49,12 +49,12 @@ def load_data_fashion_mnist(batch_size, resize=None, root='~/Datasets/FashionMNI
     if resize:
         trans.append(torchvision.transforms.Resize(size=resize))
     trans.append(torchvision.transforms.ToTensor())
-    
+
     transform = torchvision.transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
     mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
     if sys.platform.startswith('win'):
-        num_workers = 0  
+        num_workers = 0
     else:
         num_workers = 4
     train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -70,11 +70,11 @@ def evaluate_accuracy(data_iter, net, device=None):
         for X, y in data_iter:
             if isinstance(net, torch.nn.Module):
                 # set the model to evaluation mode (disable dropout)
-                net.eval() 
+                net.eval()
                 # get the acc of this batch
                 acc_sum += (net(X.to(device)).argmax(dim=1) == y.to(device)).float().sum().cpu().item()
                 # change back to train mode
-                net.train() 
+                net.train()
 
             n += y.shape[0]
     return acc_sum / n
@@ -109,14 +109,14 @@ def train(net, train_iter, test_iter, batch_size, optimizer, num_epochs, device=
 
 def main():
 
-    batch_size = 256
-    lr, num_epochs = 0.9, 100
+    batch_size = 128
+    lr, num_epochs = 0.01, 100
 
     net = AlexNet()
     optimizer = torch.optim.SGD(net.parameters(), lr=lr)
-    
+
     # load data
-    train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size)
+    train_iter, test_iter = load_data_fashion_mnist(batch_size=batch_size, resize=224)
     # train
     train(net, train_iter, test_iter, batch_size, optimizer, num_epochs)
 
