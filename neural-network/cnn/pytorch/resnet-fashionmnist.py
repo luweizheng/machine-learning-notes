85cd612f36968bb18fa4bf4dff9c35b48582d397
Lu Weizheng <luweizheng36@hotmail.com>
2020-12-02 16:53:45 +0800

    resnet

--- neural-network/cnn/pytorch/resnet-fashionmnist.py
+++ neural-network/cnn/pytorch/resnet-fashionmnist.py
@@ -8,13 +8,13 @@ import mlutils.pytorch as mlutils
 
 class Residual(nn.Module):
     """The Residual block of ResNet."""
-    def __init__(self, input_channels, num_channels,
+    def __init__(self, in_channels, num_channels,
                  use_1x1conv=False, strides=1):
         super().__init__()
-        self.conv1 = nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1, stride=strides)
+        self.conv1 = nn.Conv2d(in_channels, num_channels, kernel_size=3, padding=1, stride=strides)
         self.conv2 = nn.Conv2d(num_channels, num_channels, kernel_size=3, padding=1)
         if use_1x1conv:
-            self.conv3 = nn.Conv2d(input_channels, num_channels, kernel_size=1, stride=strides)
+            self.conv3 = nn.Conv2d(in_channels, num_channels, kernel_size=1, stride=strides)
         else:
             self.conv3 = None
         self.bn1 = nn.BatchNorm2d(num_channels)
@@ -29,24 +29,24 @@ class Residual(nn.Module):
         Y += X
         return F.relu(Y)
 
-def resnet_block(input_channels, num_channels, num_residuals, first_block=False):
+def resnet_block(in_channels, num_channels, num_residuals, first_block=False):
     """return multiple resnet blocks"""
     blk = []
     for i in range(num_residuals):
         if i == 0 and not first_block:
-            blk.append(Residual(input_channels, num_channels, use_1x1conv=True, strides=2))
+            blk.append(Residual(in_channels, num_channels, use_1x1conv=True, strides=2))
         else:
             blk.append(Residual(num_channels, num_channels))
     return blk
 
 def resnet():
     """return resnet"""
-    b1 = nn.Sequential(nn.Conv2d(input_channels=1, output_channels=64, kernel_size=7, stride=2, padding=3),
+    b1 = nn.Sequential(nn.Conv2d(in_channels=1, out_channels=64, kernel_size=7, stride=2, padding=3),
                    nn.BatchNorm2d(64), nn.ReLU(),
                    nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
-    b2 = nn.Sequential(*resnet_block(input_channels=64, num_channels=64, num_residuals=2, first_block=True))
-    b3 = nn.Sequential(*resnet_block(input_channels=64, num_channels=128, num_residuals=2))
-    b4 = nn.Sequential(*resnet_block(input_channels=128, num_channels=256, num_residuals=2))
+    b2 = nn.Sequential(*resnet_block(in_channels=64, num_channels=64, num_residuals=2, first_block=True))
+    b3 = nn.Sequential(*resnet_block(in_channels=64, num_channels=128, num_residuals=2))
+    b4 = nn.Sequential(*resnet_block(in_channels=128, num_channels=256, num_residuals=2))
     b5 = nn.Sequential(*resnet_block(256, 512, 2))
 
     net = nn.Sequential(b1, b2, b3, b4, b5,
